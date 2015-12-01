1fe7b9a02a10ee46390dfc43b90ef1dd3142021a
Nikolaj Brinch Jørgensen <nikolajbrinch@gmail.com>
2015-12-01 21:17:04 +0100

    Running grade setup

---
+++ app-framework/src/main/java/org/diylc/appframework/images/IconLoader.java
@@ -0,0 +1,35 @@
+package org.diylc.appframework.images;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Loads image resources as Icons.
+ * 
+ * @author Branislav Stojkovic
+ */
+public enum IconLoader {
+
+	Error("error.png"), Warning("warning.png"), LightBulbOn("lightbulb_on.png"), LightBulbOff(
+			"lightbulb_off.png"), MoveSmall("move_small.png"), Undo("undo.png"), Redo("redo.png"), ;
+
+	private static final Logger LOG = Logger.getLogger(IconLoader.class);
+
+	protected String name;
+
+	private IconLoader(String name) {
+		this.name = name;
+	}
+
+	public Icon getIcon() {
+		java.net.URL imgURL = getClass().getResource("/icons/" + name);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL, name);
+		} else {
+			LOG.error("Couldn't find file: " + imgURL);
+			return null;
+		}
+	}
+}
